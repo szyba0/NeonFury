f043db1a051eb14dd67ea438a3c930f7cc26a0f8
Michał Duszak <62519755+micdusz@users.noreply.github.com>
2024-10-26 22:24:25 +0200

    catching up

---
+++ scripts/bullet_01.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@export var speed = 500
+
+var direction = 0.0
+var spawnPos = Vector2.ZERO
+var spawnRot = 0.0
+
+func _ready():
+	global_position = spawnPos
+	rotation = spawnRot
+
+func _physics_process(delta):
+	velocity = Vector2.RIGHT.rotated(direction) * speed
+	move_and_slide()
+
+func _on_Area2D_body_entered(body: Node):
+	print("HIT!")
+	queue_free()
+
+func _on_life_timeout():
+	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,55 @@
 extends CharacterBody2D
 
-var speed = 400
+@onready var main = get_tree().get_root().get_node("Main")
+@onready var bullet = load("res://scenes/bullet_01.tscn")
+@onready var ammo_bar = $"/root/Main/AmmoBar" 
+
+@export var speed = 400
+var max_ammo: int
+var current_ammo: int
+
+var weapon_type:String = "firearm"
+var current_weapon:String
+
+func _ready():
+	max_ammo = ammo_bar.max_value
+	current_ammo = max_ammo
+	update_ammo_bar()
 
 func read_input():
 	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
 	velocity = input_direction * speed
 	look_at(get_global_mouse_position())
-	
+
+func _input(_event):
+	if Input.is_action_just_pressed("LMB"): 
+		match weapon_type:
+			#"fists":
+				#fisticuffs()
+			#"melee":
+				#melee()
+			"firearm":
+				shoot()
+			#_:
+				#cry()
+		shoot()
 
 func _physics_process(_delta):
 	read_input()
 	move_and_slide()
+
+func shoot():
+	if current_ammo > 0:
+		var instance = bullet.instantiate()
+		instance.direction = rotation
+		instance.spawnPos = global_position
+		instance.spawnRot = global_rotation
+		main.add_child(instance)
+		
+		current_ammo -= 1
+		update_ammo_bar()
+	else:
+		print("Out of ammo!")
+
+func update_ammo_bar():
+	ammo_bar.value = current_ammo
