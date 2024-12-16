66808ebd92112a8f99931ac38c9d3a563a980535
Qestes <62556821+szyba0@users.noreply.github.com>
2024-12-16 18:08:18 +0100

    imma kms

--- scenes/Pause.gd
+++ scenes/Pause.gd
@@ -11,9 +11,17 @@ func _process(_delta: float) -> void:
 	pass
 
 func _input(_event):
-			
 	if Input.is_action_just_pressed("PAUSE"):
 		get_viewport().set_input_as_handled()
+		print("b")
 		if(get_tree().paused):
-			$"/root/Main/Player/PauseNode/PauseMenu/Control".hide()
+			$".".hide()
 			get_tree().paused = false
+
+func _on_resume_button_pressed() -> void:
+	$".".hide()
+	get_tree().paused = false
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
