e3128a88ec8cadaee67e40b83c56b2873373c05c
Qestes <62556821+szyba0@users.noreply.github.com>
2025-01-10 21:24:30 +0100

    main menu added

---
+++ scripts/main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$VBoxContainer/StartButton.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
